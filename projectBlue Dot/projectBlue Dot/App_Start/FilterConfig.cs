635f0045097708bb6cee07f9badcb375fdef096e
shaileshsinghjack <shaileshsingh501122@gmail.com>
2017-06-30 15:55:14 +0530

    thats one hof

---
+++ projectBlue Dot/projectBlue Dot/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace projectBlue_Dot
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ projectBlue Dot/projectBlue Dot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="projectBlue_Dot.MvcApplication" Language="C#" %>
